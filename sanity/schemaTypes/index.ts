f6d82df971d57c0c12791eb9ebb4c6992c256f26
oceanasrat <wykiyanosa@gmail.com>
2026-04-04 09:22:04 -0500

    Update index.ts

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,11 @@
 import product from "./product"
 import promoBanner from "./promoBanner"
 import order from "./order"
+import storeSettings from "./storeSettings" // NEW
 
-export const schemaTypes = [product, promoBanner, order]
+export const schemaTypes = [
+  product,
+  promoBanner,
+  order,
+  storeSettings // NEW
+]
